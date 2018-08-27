7135911c91272a13b31548a01d7dcfdba2f66c60
Joey Joy <moejo@umich.edu>
2018-08-26 20:08:08 -0400

    first Commie

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,54 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
+
+import { TodoProvider } from "../../providers/todo/todo";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public todos = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(private todoProv: TodoProvider, public navCtrl: NavController, private alertController: AlertController) {
+    this.todos = this.todoProv.getTodos();
+  }
 
+  openTodoAlert(){
+    let addToDoAlert = this.alertController.create({
+      title: "Add a todo",
+      message: "Enter your todo",
+      inputs: [
+        {
+          type: "text",
+          name: "addTodoInput"
+        }
+      ],
+      buttons: [
+        {
+          text: "Remove",
+          handler: ()=>{
+            while(this.todos.length>0){
+             this.todos.pop();
+            }
+          }
+        },
+        {
+           text: "Cancel" 
+        },
+        {
+          text: "Add Todo",
+          handler: (inputData)=> {
+            let todoText;
+            todoText = inputData.addTodoInput;
+            //this.todos.push(todoText);
+            this.todoProv.addTodo(todoText);
+          }
+        }
+      ]
+    });
+
+    addToDoAlert.present();
   }
 
 }
